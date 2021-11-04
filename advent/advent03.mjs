cdcdb37765f298a896b671dde6684bcd88e2ad96
mikeron <themike7171@gmail.com>
2021-11-04 12:26:50 -0400

    advent03  part 1 fonctionnel

--- advent/advent03.mjs
+++ advent/advent03.mjs
@@ -3,13 +3,40 @@ import * as fs from "fs";
 const contenu = fs.readFileSync("./data03.txt", "utf-8");
 
 //separrer mes lignes
-const contenuSplit = contenu.split("\r\n");
+const contenuSplit = contenu.split("\n");
 
-console.table(contenuSplit);
-//rendre les lignes plus longue?
 
-//console.log(lign.repeat(4));
 
 // calculateTree compte le nombre d'arbre
 
-function calculateTree() {}
+function calculateTree() {
+
+// sortir element d'une ligne ??
+//lign.slice(0,1)
+
+
+// sortir element de tous les lignes  ??
+let count = 0
+for (let i = 0; i < contenuSplit.length; i++) {
+ const ligne = contenuSplit[i];
+ let position = [3 * i]
+ let realPos = position % 31
+ const element = ligne.charAt(realPos)
+ //console.log('en ligne ' + [i] + ' element: ' + element)
+ 
+ //mettre un decompte dans boucle
+ 
+ 
+ 
+ 
+ 
+ // si l'element = # compter +1
+ if (element === '#'){
+     count ++
+    }
+}
+console.log(count)
+}
+
+
+calculateTree()
