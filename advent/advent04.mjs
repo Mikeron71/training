68285a8755ed4f1015ddce79caddd0ef7f0ac54d
Michael  Rondeau <themike7171@gmail.com>
2021-11-06 08:39:05 -0400

    test

--- advent/advent04.mjs
+++ advent/advent04.mjs
@@ -3,5 +3,7 @@ import * as fs from "fs";
 const contenu = fs.readFileSync("./data04.txt", "utf-8");
 
 //separrer mes lignes
-const contenuSplit = contenu.split("\n\n");
-console.table(contenuSplit);
+const passeport = contenu.split("\n\r");
+const requiredFields = console.table(passeport);
+
+//creer mes object de passport
