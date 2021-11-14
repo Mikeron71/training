8739f10869ab74a492a8ed6ece01c9f50b9acc1c
Carl St-Laurent <carl@carlstlaurent.com>
2021-11-13 20:57:40 -0500

    Advent 04

--- advent/advent04.mjs
+++ advent/advent04.mjs
@@ -1,33 +1,55 @@
 import * as fs from "fs";
 
-const contenu = fs.readFileSync("./data04.txt", "utf-8");
+const contenu = fs.readFileSync("./data04-carl.txt", "utf-8");
 
 //separrer mes lignes
 const contenuSplit = contenu.split("\n\n");
 //console.table(contenuSplit)
 
-let passeportTest = contenuSplit[0]
+const result = contenuSplit.map(decodePasseport).filter(x=> x === true).length
 
 //console.log(passeportTest)
 
 //creer  un objet avec tous les parametres
 
- let passeport0 = passeportTest.split('\n')
- //console.log(passeport0)
 
- //mettre le passeport sur une string?
-
-let stringpasseport = passeport0.toString();
-//console.log(stringpasseport)
-
-let donePasseport = stringpasseport.replaceAll(' ', ',')
 //console.log(donePasseport)
 
-//test objects
-let pass = {
-    donePasseport
-}
 
-console.log(
+console.log('Result', result)
 
 //verifier si les passeports contiennent tous les objets
+
+// byr (Birth Year)
+// iyr (Issue Year)
+// eyr (Expiration Year)
+// hgt (Height)
+// hcl (Hair Color)
+// ecl (Eye Color)
+// pid (Passport ID)
+// cid (Country ID)
+function decodePasseport(passeportString) {
+    let passeport0 = passeportString.
+    replaceAll('\n', ' ').
+    split(' ').
+    map((t) => t.split(':'))
+    
+   const good = new Map(passeport0)
+
+   if (
+    hasAll(good, ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl','pid'])
+   ) {
+       return true
+   }
+   return false
+
+}
+
+
+function hasAll(mapObject,p){ 
+for (const i of p) {
+    const has = mapObject.has(i)
+    if (has === false ) return false
+}
+return true
+}
